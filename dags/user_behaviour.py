4550b80c75a684f84943d8528598157268f2d571
Mark <mark.a.mcadam@gmail.com>
2020-10-20 14:31:44 -0700

    stage 1 and stage 2 tested and working

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -64,40 +64,40 @@ def remove_local_file(filelocation):
         logging.info(f"File {filelocation} not found")
 
 
-# pg_unload = PostgresOperator(
-#     dag=dag,
-#     task_id="pg_unload",
-#     sql=unload_user_purchase,
-#     postgres_conn_id="postgres_default",
-#     params={"temp_filtered_user_purchase": temp_filtered_user_purchase},
-#     depends_on_past=True,
-#     wait_for_downstream=True,
-# )
+pg_unload = PostgresOperator(
+    dag=dag,
+    task_id="pg_unload",
+    sql=unload_user_purchase,
+    postgres_conn_id="postgres_default",
+    params={"temp_filtered_user_purchase": temp_filtered_user_purchase},
+    depends_on_past=True,
+    wait_for_downstream=True,
+)
 
-# user_purchase_to_s3_stage = PythonOperator(
-#     dag=dag,
-#     task_id="user_purchase_to_s3_stage",
-#     python_callable=_local_to_s3,
-#     op_kwargs={
-#         "filename": temp_filtered_user_purchase,
-#         "key": temp_filtered_user_purchase_key,
-#     },
-# )
+user_purchase_to_s3_stage = PythonOperator(
+    dag=dag,
+    task_id="user_purchase_to_s3_stage",
+    python_callable=_local_to_s3,
+    op_kwargs={
+        "filename": temp_filtered_user_purchase,
+        "key": temp_filtered_user_purchase_key,
+    },
+)
 
-# remove_local_user_purchase_file = PythonOperator(
-#     dag=dag,
-#     task_id="remove_local_user_purchase_file",
-#     python_callable=remove_local_file,
-#     op_kwargs={"filelocation": temp_filtered_user_purchase,},
-# )
+remove_local_user_purchase_file = PythonOperator(
+    dag=dag,
+    task_id="remove_local_user_purchase_file",
+    python_callable=remove_local_file,
+    op_kwargs={"filelocation": temp_filtered_user_purchase,},
+)
 
 
-# end_of_data_pipeline = DummyOperator(task_id="end_of_data_pipeline", dag=dag)
+end_of_data_pipeline = DummyOperator(task_id="end_of_data_pipeline", dag=dag)
 
 
 # Step 1
 # data pipeline
-# pg_unload >> user_purchase_to_s3_stage >> remove_local_user_purchase_file >> end_of_data_pipeline
+pg_unload >> user_purchase_to_s3_stage >> remove_local_user_purchase_file >> end_of_data_pipeline
 
 
 # Step 2 Code
