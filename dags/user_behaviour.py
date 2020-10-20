61615e749cbd5b9afd8a260578f14d0b28a48725
Mark <mark.a.mcadam@gmail.com>
2020-10-20 11:52:57 -0700

    Auto stash before merge of "main" and "origin/main"

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -1,15 +1,28 @@
 from datetime import datetime, timedelta
-
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.hooks.S3_hook import S3Hook
+from airflow.operators import PythonOperator
+import os
 
 # config
 # local
 unload_user_purchase = "./scripts/sql/filter_unload_user_purchase.sql"
 temp_filtered_user_purchase = "/temp/temp_filtered_user_purchase.csv"
+movie_review_local = "/data/movie_review/movie_review.csv"  # location of movie review within the docker container
+# look at the docker-compose volume mounting for clarification
 
+
+# remote config
+BUCKET_NAME = "data-engineering-batch-mmc"
+temp_filtered_user_purchase_key = (
+    "user_purchase/stage/{{ ds }}/temp_filtered_user_purchase.csv"
+)
+movie_review_load = "movie_review/load/movie.csv"
+
+
+# code for everything
 default_args = {
     "owner": "airflow",
     "depends_on_past": True,
@@ -23,6 +36,21 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
+
+# helper function(s)
+
+
+def _local_to_s3(filename, key, bucket_name=BUCKET_NAME):
+    s3 = S3Hook()
+    s3.load_file(filename=filename, bucket_name=bucket_name, replace=True, key=key)
+
+
+def remove_local_file(filelocation):
+    if os.path.isfile(filelocation):
+        os.remove(filelocation)
+    else:
+        logging.info(f"File {filelocation} not found")
+
 
 dag = DAG(
     "user_behaviour",
@@ -43,6 +71,33 @@ pg_unload = PostgresOperator(
     wait_for_downstream=True,
 )
 
+user_purchase_to_s3_stage = PythonOperator(
+    dag=dag,
+    task_id="user_purchase_to_s3_stage",
+    python_callable=_local_to_s3,
+    op_kwargs={
+        "filename": temp_filtered_user_purchase,
+        "key": temp_filtered_user_purchase_key,
+    },
+)
 
-# this controls flow of data through the pipeline via dags in airflow
-pg_unload >> end_of_data_pipeline
+remove_local_user_purchase_file = PythonOperator(
+    dag=dag,
+    task_id="remove_local_user_purchase_file",
+    python_callable=remove_local_file,
+    op_kwargs={"filelocation": temp_filtered_user_purchase,},
+)
+
+movie_review_to_s3_stage = PythonOperator(
+    dag=dag,
+    task_id="movie_review_to_s3_stage",
+    python_callable=_local_to_s3,
+    op_kwargs={"filename": movie_review_local, "key": movie_review_load,},
+)
+
+
+# data pipeline
+pg_unload >> user_purchase_to_s3_stage >> remove_local_user_purchase_file >> end_of_data_pipeline
+
+# file -> s3 -> EMR -> s3
+movie_review_to_s3_stage
